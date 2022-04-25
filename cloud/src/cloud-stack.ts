d36102d643d773174446bb60706703312d2ed071
Brandon Vicedomini <brandonvio@outlook.com>
2022-04-24 23:01:42 -0700

    Added admin bucket, subdomain, etc.

---
+++ cloud/src/cloud-stack.ts
@@ -0,0 +1,22 @@
+import * as cdk from 'aws-cdk-lib';
+import { Stack, StackProps } from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import * as origin from './web';
+
+export class CloudStack extends Stack {
+    constructor(scope: Construct, id: string, props?: StackProps) {
+        super(scope, id, props);
+
+        const certificateArn = cdk.Fn.importValue(
+            'rythm-origin-certificatearn',
+        );
+        const hostedZoneId = cdk.Fn.importValue('rythm-origin-hostedzoneid');
+
+        const rythmOrigin = new origin.Web(this, 'AdminWeb', {
+            name: 'Admin',
+            domain: 'rythm.cc',
+            certificateArn: certificateArn,
+            hostedZoneId: hostedZoneId,
+        });
+    }
+}
---
+++ cloud/src/cloud.ts
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+import 'source-map-support/register';
+import * as cdk from 'aws-cdk-lib';
+import { CloudStack } from './cloud-stack';
+
+const app = new cdk.App();
+new CloudStack(app, 'RythmAdminCloudStack', {
+    stackName: 'rythm-admin-cloud-stack',
+    env: {
+        account: process.env.AWS_TARGET_ACCOUNT,
+        region: process.env.AWS_REGION,
+    },
+});
